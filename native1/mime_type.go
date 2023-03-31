dc2190780877fa7de97c59c2f6551f7e915f9e5c
Kris Liu <kris.liu@farfetch.com>
2023-03-31 20:04:39 +0800

    Add more mime types

--- native1/mime_type.go
+++ native1/mime_type.go
@@ -6,55 +6,90 @@ import (
 
 type (
 	ImageMimeType string
+	AudioMimeType string
 	VideoMimeType string
 )
 
 const (
-	ImageMimeTypeBMP  ImageMimeType = "image/bmp"
-	ImageMimeTypeGIF  ImageMimeType = "image/gif"
-	ImageMimeTypeHEIC ImageMimeType = "image/heic"
-	ImageMimeTypeJPEG ImageMimeType = "image/jpeg"
-	ImageMimeTypePNG  ImageMimeType = "image/png"
-	ImageMimeTypeSVG  ImageMimeType = "image/svg+xml"
-	ImageMimeTypeTIFF ImageMimeType = "image/tiff"
-	ImageMimeTypeWEBP ImageMimeType = "image/webp"
+	ImageMimeTypeAVIF ImageMimeType = "image/avif"         // avif
+	ImageMimeTypeGIF  ImageMimeType = "image/gif"          // gif
+	ImageMimeTypeHEIC ImageMimeType = "image/heic"         // heic
+	ImageMimeTypeJPEG ImageMimeType = "image/jpeg"         // jpeg jpg
+	ImageMimeTypePNG  ImageMimeType = "image/png"          // png
+	ImageMimeTypeSVG  ImageMimeType = "image/svg+xml"      // svg svgz
+	ImageMimeTypeTIFF ImageMimeType = "image/tiff"         // tif tiff
+	ImageMimeTypeWBMP ImageMimeType = "image/vnd.wap.wbmp" // wbmp
+	ImageMimeTypeWEBP ImageMimeType = "image/webp"         // webp
+	ImageMimeTypeICO  ImageMimeType = "image/x-icon"       // ico
+	ImageMimeTypeJNG  ImageMimeType = "image/x-jng"        // jng
+	ImageMimeTypeBMP  ImageMimeType = "image/x-ms-bmp"     // bmp
 
-	VideoMimeTypeASF  VideoMimeType = "video/x-ms-asf"
-	VideoMimeTypeAVI  VideoMimeType = "video/x-msvideo"
-	VideoMimeTypeMP4  VideoMimeType = "video/mp4"
-	VideoMimeTypeMPEG VideoMimeType = "video/mpeg"
-	VideoMimeTypeMOV  VideoMimeType = "video/quicktime"
-	VideoMimeTypeOGG  VideoMimeType = "video/ogg"
-	VideoMimeTypeWEBM VideoMimeType = "video/webm"
-	VideoMimeTypeWMV  VideoMimeType = "video/x-ms-wmv"
+	AudioMimeTypeMID AudioMimeType = "audio/midi"        // mid midi kar
+	AudioMimeTypeMP3 AudioMimeType = "audio/mpeg"        // mp3
+	AudioMimeTypeOGG AudioMimeType = "audio/ogg"         // ogg
+	AudioMimeTypeM4A AudioMimeType = "audio/x-m4a"       // m4a
+	AudioMimeTypeRA  AudioMimeType = "audio/x-realaudio" // ra
+
+	VideoMimeType3GP  VideoMimeType = "video/3gpp"      // 3gpp 3gp
+	VideoMimeTypeTS   VideoMimeType = "video/mp2t"      // ts
+	VideoMimeTypeMP4  VideoMimeType = "video/mp4"       // mp4
+	VideoMimeTypeMPEG VideoMimeType = "video/mpeg"      // mpeg mpg
+	VideoMimeTypeMOV  VideoMimeType = "video/quicktime" // mov
+	VideoMimeTypeWEBM VideoMimeType = "video/webm"      // webm
+	VideoMimeTypeFLV  VideoMimeType = "video/x-flv"     // flv
+	VideoMimeTypeM4V  VideoMimeType = "video/x-m4v"     // m4v
+	VideoMimeTypeMNG  VideoMimeType = "video/x-mng"     // mng
+	VideoMimeTypeASF  VideoMimeType = "video/x-ms-asf"  // asx asf
+	VideoMimeTypeWMV  VideoMimeType = "video/x-ms-wmv"  // wmv
+	VideoMimeTypeAVI  VideoMimeType = "video/x-msvideo" // avi
 )
 
 var (
 	AllImageMimeTypes = []ImageMimeType{
-		ImageMimeTypeBMP,
+		ImageMimeTypeAVIF,
 		ImageMimeTypeGIF,
 		ImageMimeTypeHEIC,
 		ImageMimeTypeJPEG,
 		ImageMimeTypePNG,
 		ImageMimeTypeSVG,
 		ImageMimeTypeTIFF,
+		ImageMimeTypeWBMP,
 		ImageMimeTypeWEBP,
+		ImageMimeTypeICO,
+		ImageMimeTypeJNG,
+		ImageMimeTypeBMP,
+	}
+
+	AllAudioMimeTypes = []AudioMimeType{
+		AudioMimeTypeMID,
+		AudioMimeTypeMP3,
+		AudioMimeTypeOGG,
+		AudioMimeTypeM4A,
+		AudioMimeTypeRA,
 	}
 
 	AllVideoMimeTypes = []VideoMimeType{
+		VideoMimeType3GP,
+		VideoMimeTypeTS,
 		VideoMimeTypeASF,
 		VideoMimeTypeAVI,
 		VideoMimeTypeMP4,
 		VideoMimeTypeMPEG,
 		VideoMimeTypeMOV,
-		VideoMimeTypeOGG,
 		VideoMimeTypeWEBM,
+		VideoMimeTypeFLV,
+		VideoMimeTypeM4V,
+		VideoMimeTypeMNG,
 		VideoMimeTypeWMV,
 	}
 )
 
 func (e ImageMimeType) IsValid() error {
 	return vld.EnumIsValid(&e, AllImageMimeTypes)
+}
+
+func (e AudioMimeType) IsValid() error {
+	return vld.EnumIsValid(&e, AllAudioMimeTypes)
 }
 
 func (e VideoMimeType) IsValid() error {
