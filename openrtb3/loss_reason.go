314461f797b00ba4da7ba54ca7e3901886f045df
Kris Liu <kris.liu@farfetch.com>
2023-04-24 18:57:38 +0800

    Add loss reason

--- openrtb3/loss_reason.go
+++ openrtb3/loss_reason.go
@@ -16,10 +16,11 @@ const (
 	LossInvalidDealID           LossReason = 4   // Invalid Deal ID
 	LossInvalidAuctionID        LossReason = 5   // Invalid Auction ID
 	LossInvalidAdvertiserDomain LossReason = 6   // Invalid Advertiser Domain
-	LossMissingMarkup           LossReason = 7   // Missing Markup
+	LossMissingMarkup           LossReason = 7   // Missing Markup (Adm)
 	LossMissingCreativeID       LossReason = 8   // Missing Creative ID
 	LossMissingBidPrice         LossReason = 9   // Missing Bid Price
 	LossMissingApproval         LossReason = 10  // Missing Minimum Creative Approval Data
+	LossInvalidMarkup           LossReason = 11  // Invalid Markup (Adm)
 	LossBelowAuctionFloor       LossReason = 100 // Bid was Below Auction Floor
 	LossBelowDealFloor          LossReason = 101 // Bid was Below Deal Floor
 	LossLostToHigherBid         LossReason = 102 // Lost to Higher Bid
@@ -41,4 +42,7 @@ const (
 	LossNotAllowedInDeal        LossReason = 213 // Creative Filtered - Not Allowed in Deal
 	LossInvalidSKAdNetwork      LossReason = 214 // Creative Filtered - Invalid SKAdNetwork
 	LossAppBundleExclusions     LossReason = 215 // Creative Filtered - App Bundle Exclusions
+	LossUnsupportedAssetType    LossReason = 216 // Creative Filtered - Unsupported Asset Type
+	LossUnsupportedMimeType     LossReason = 217 // Creative Filtered - Unsupported Mime Type
+	LossBitRateNotAllowed       LossReason = 218 // Creative Filtered - Bit Rate Not Allowed
 )
